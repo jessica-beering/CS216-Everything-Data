f1e4e57cd26ea8fade532fe01d7d52b6f8138eab
Jess Beering <jessica.beering@duke.edu>
2018-01-29 11:52:50 -0500

    Delete 2D.sql

--- hw02/2D.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW NC_members AS
-SELECT p.id, p.first_name, p.last_name, 
-	r.type, r.state
-FROM persons p, person_roles r
-WHERE p.id = r.person_id AND state = 'NC';
-
-SELECT*
-FROM NC_members; 
